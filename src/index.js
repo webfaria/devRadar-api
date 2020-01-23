12efbe040c3ed543c75cfb762e72082e7784bd84
webfaria <web_faria@yahoo.com.br>
2020-01-22 22:03:08 -0300

    Liberando acesso externo por meio do cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://webfaria:v6d3v6@cluster0-irhas.mongodb.net/week1
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
